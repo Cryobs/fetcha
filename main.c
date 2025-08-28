4ac9ac12cf76dd1ec7c838bf7702f3d69b58f25d
cryobs <cryobs.ads@gmail.com>
2025-08-28 07:46:33 +0200

    ADD: base print ascii with info

--- main.c
+++ main.c
@@ -1,42 +1,146 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define ASCII_DIR "config/ascii.txt"
 
-int
-print_ascii()
+
+#define ASCII_DIR     "config/ascii.txt"
+#define ASCII_COLOR   "32"
+
+#define FIRST_COLOR   "37"
+#define SECOND_COLOR  "32"
+
+#define USER_HOST_SEP "@"
+
+#define USER          "brace"
+#define HOST          "arch"
+
+
+struct ascii 
 {
+  char *art; 
+  int width;
+  int height;
+};
+
+void
+free_ascii(struct ascii *s)
+{
+  if (!s) return;
+  free(s->art);
+  s->art = NULL;
+}
+
+
+void
+print_color(char *s, char *color)
+{
+  printf("\x1b[%sm%s\x1b[0m", color, s);
+}
+
+
+void
+get_ascii_size(struct ascii *res)
+{
+  if (!res || !res->art) {
+    res->width = res->height = 0;
+    return;
+  }
+
+  int maxw = 0;
+  int curw = 0;
+  int lines = 0;
+  char *p = res->art;
+  if  (*p == '\0') {
+    res->width = res->height = 0;
+    return;
+  }
+
+  while (*p) {
+    if (*p == '\n') {
+      lines++;
+      if (curw > maxw) maxw = curw;
+      curw = 0;
+    } else {
+      curw++;
+    }
+    p++;
+  }
+
+  /* if last char is not '\n' */
+  if (curw == 0 || (res->art[0] != '\0' && res->art[0] == '\n')) {
+    lines++;
+    if (curw > maxw) maxw = curw;
+  }
+
+  res->width = maxw;
+  res->height = lines;
+}
+
+
+struct ascii
+get_ascii()
+{
+  struct ascii res;
   FILE* fp = fopen(ASCII_DIR, "r") ;
-  if (!fp) 
-    return -1;
+  if (!fp) {
+    perror("ASCII");
+    exit(EXIT_FAILURE);
+  }
   
   struct stat st;
   if(fstat(fileno(fp), &st) == 0 && S_ISREG(st.st_mode)) {
     size_t sz = (size_t)st.st_size;
-    char *buf = malloc(sz + 1);
-    if (!buf) 
+    res.art = malloc(sz + 1);
+    if (!res.art) 
       fclose(fp);
     
-    size_t read = fread(buf, 1, sz, fp);
-    buf[read] = '\0';
+    size_t read = fread(res.art, 1, sz, fp);
+    res.art[read] = '\0';
     fclose(fp);
-    puts(buf);
-    free(buf);
-    return 0;
   }
-  return 1;
-
-
-
+  get_ascii_size(&res);
+  return res;
 }
 
+int
+print_fetch(struct ascii *res)
+{
+  char *p = res->art;
+  int  curw = 0;
+  while(*p) {
+    if(*p != '\n'){
+      putchar(*p);
+      p++;
+      curw++;
+      continue;
+    }
+    while(curw != res->width) {
+      putchar(' ');
+      curw++;
+    }
+
+    /*  print info */
+
+    printf("INFO"); 
+
+    p++;
+    curw = 0;
+    printf("\n");
+  }
+  return 0;
+}
 
 int
 main(int argc, char *argv[])
 {
-  print_ascii(); 
+  struct ascii art = get_ascii();
+  //puts(art.art); 
+  
+  print_fetch(&art);
 
+  free_ascii(&art);
   return 0;
 
 
